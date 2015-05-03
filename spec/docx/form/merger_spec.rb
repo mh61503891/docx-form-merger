2d96042a0ff7048bd4df924d3f8898732034e25e
mh61503891 <mh.on.web@gmail.com>
2015-05-04 00:36:46 +0900

    bundle gem docx-form-merger -t

---
+++ spec/docx/form/merger_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Docx::Form::Merger do
+  it 'has a version number' do
+    expect(Docx::Form::Merger::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
