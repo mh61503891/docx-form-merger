15fdd930c20619f66f690f7c4dcbdf126505dfdb
mh61503891 <mh.on.web@gmail.com>
2015-05-04 00:22:23 +0900

    bundle gem docx-form-merger -

---
+++ lib/docx/form/merger.rb
@@ -0,0 +1,9 @@
+require "docx/form/merger/version"
+
+module Docx
+  module Form
+    module Merger
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/docx/form/merger/version.rb
@@ -0,0 +1,7 @@
+module Docx
+  module Form
+    module Merger
+      VERSION = "0.1.0"
+    end
+  end
+end
